822bed1c14d7263084084bf658a9df5c5f4525e8
M.Raşit OKUMUŞ <hi@okumus.dev>
2026-05-12 13:34:53 +0300

    feat/nvim: catpuchin theme

--- home/dot_config/nvim/lua/plugins/ui.lua
+++ home/dot_config/nvim/lua/plugins/ui.lua
@@ -1,3 +1,47 @@
 return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "frappe",
+				transparent_background = false,
+				term_colors = true,
+				styles = {
+					comments = { "italic" },
+					keywords = { "italic" },
+				},
+				integrations = {
+					telescope = true,
+					treesitter = true,
+					gitsigns = true,
+					mason = true,
+					native_lsp = {
+						enabled = true,
+						underlines = {
+							errors = { "underline" },
+							hints = { "underline" },
+							warnings = { "underline" },
+							information = { "underline" },
+						},
+					},
+				},
+			})
+			vim.cmd.colorscheme("catppuccin-frappe")
+		end,
+	},
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		opts = {
+			options = {
+				theme = "catppuccin",
+				component_separators = "│",
+				section_separators = "",
+			},
+		},
+	},
 }
