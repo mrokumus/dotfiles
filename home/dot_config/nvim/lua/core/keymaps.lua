4cda126ac167b0e0c27fbde87a1dc01ab9954911
M.Raşit OKUMUŞ <hi@okumus.dev>
2026-06-16 10:55:23 +0300

    feat/nvim: remove non default bindings

--- home/dot_config/nvim/lua/core/keymaps.lua
+++ home/dot_config/nvim/lua/core/keymaps.lua
@@ -5,12 +5,12 @@ local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- Stay in indent mode when shifting
-map("v", "<", "<gv", opts)
-map("v", ">", ">gv", opts)
+-- map("v", "<", "<gv", opts)
+-- map("v", ">", ">gv", opts)
 
 -- Move selected lines up/down
-map("v", "J", ":m '>+1<CR>gv=gv", opts)
-map("v", "K", ":m '<-2<CR>gv=gv", opts)
+-- map("v", "J", ":m '>+1<CR>gv=gv", opts)
+-- map("v", "K", ":m '<-2<CR>gv=gv", opts)
 
 -- Clear search highlighting
 map("n", "<leader>nh", ":nohlsearch<CR>", { desc = "Clear search highlight" })
